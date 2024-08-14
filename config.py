a629ab8a28333dcae36555161b9fa87d2bf7017b
zy1305 <psp.fongzeyeung@gmail.com>
2024-08-14 23:12:16 +0800

    [ops] added one perm duty cmd

--- config.py
+++ config.py
@@ -72,5 +72,6 @@ PERM_DUTY_CMDS = {"3SGZEYEUNG":os.getenv("ZEYEUNG_NUMBER"),
                   "JEREMIAH":os.getenv("JEREMIAH_NUMBER"), 
                   "DAEMON":os.getenv("DAEMON_NUMBER"), 
                   "MAX":os.getenv("MAX_NUMBER"),
-                  "AJLOY":os.getenv("AJLOY_NUMBER")
+                  "AJLOY":os.getenv("AJLOY_NUMBER"),
+                  "GAOSHAN":os.getenv("GAOSHAN_NUMBER")
 }
