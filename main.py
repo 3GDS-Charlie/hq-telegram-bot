3b02f60c527dc7184de4a31a9f6ee3f4f53c4adc
zy1305 <psp.fongzeyeung@gmail.com>
2024-08-22 18:56:43 +0800

    [ops] minor fixes

--- main.py
+++ main.py
@@ -1467,7 +1467,7 @@ async def location(update: Update, context: CallbackContext) -> int:
                 continue
             if not foundDescription: continue
             if line == "": continue
-            if description is None or line.replace("*", "").replace(" ", "").replace(":", "") != "6)CurrentStatus":
+            if description is None or (not foundStatus and line.replace("*", "").replace(" ", "").replace(":", "") != "6)CurrentStatus"):
                 if description is None: description = line
                 else: description = description + "\n\n" + line
                 context.user_data['description'] = description
